008baac556a5064709e91fc56aee185b2a727201
bence04 <tothbence95@gmail.com>
2018-03-04 15:10:06 +0100

    edit gulp file

--- HF/gulpfile.js
+++ HF/gulpfile.js
@@ -3,12 +3,25 @@ var sass = require("gulp-sass");
 var htmlmin = require("gulp-htmlmin");
 var cssmin = require("gulp-cssmin");
 var rename = require("gulp-rename");
+var minifyjs = require('gulp-js-minify');
 var browserSync = require("browser-sync").create();
 
-gulp.task("copy_materialize", function() {
+gulp.task("copy-materialize", function() {
   return gulp
     .src("node_modules/materialize-css/dist/**/*")
     .pipe(gulp.dest("build/assets/"));
+});
+
+gulp.task("copy-js", function() {
+  return gulp
+    .src("app/js/**/*.js")
+    .pipe(gulp.dest("build/assets/js"));
+});
+
+gulp.task("copy-img", function() {
+  return gulp
+    .src("app/img/**/*")
+    .pipe(gulp.dest("build/assets/img"));
 });
 
 gulp.task("sass", function() {
@@ -22,12 +35,23 @@ gulp.task("sass", function() {
 });
 
 gulp.task("minify-css", function() {
+  return gulp
+  .src("app/style/**/*.scss")
+  .pipe(sass())
+  .pipe(cssmin())
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(gulp.dest("build/assets/css"))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
+});
+
+gulp.task("minify-js", function() {
     return gulp
-    .src("app/style/**/*.scss")
-    .pipe(sass())
-    .pipe(cssmin())
+    .src("app/js/**/*.js")
+    .pipe(minifyjs())
     .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("build/assets/css"))
+    .pipe(gulp.dest("build/assets/js"))
     .pipe(browserSync.reload({
       stream: true
     }));
@@ -52,11 +76,13 @@ gulp.task("browserSync", function() {
   });
 });
 
-gulp.task("watch", ["browserSync", "sass", "src", "minify-css", "copy_materialize"], function() {
+gulp.task("watch", ["browserSync", "sass", "src", "minify-css", "copy-materialize", "copy-js", "minify-js", "copy-img"], function() {
   gulp.watch("app/style/**/*.scss", ["sass", "minify-css"]);
+  gulp.watch("app/js/**/*.js", ["copy-js", "minify-js"]);
+  gulp.watch("app/img/**/*", ["copy-img"]);
   gulp.watch("app/src/**/*.html", ["src"]);
 });
 
-gulp.task("build", ["sass", "src", "minify-css", "copy_materialize"], function() {
+gulp.task("build", ["sass", "src", "minify-css", "copy-materialize", "copy-js", "minify-js", "copy-img"], function() {
   console.log("Building files");
 });
--- HF/index.js
+++ HF/index.js
@@ -5,4 +5,5 @@ var app = express();
 app.use(express.static ('build'));
 var server = app.listen(3000, function() {
     console.log("listening on localhost:"+"3001".green);
+    console.log("It's works".green);
 });
